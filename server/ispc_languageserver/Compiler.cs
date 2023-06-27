541f294c025abe917359cdedd6935ea6caad3af2
ethandav <ethan.davis@intel.com>
2023-06-26 19:46:19 -0700

    Updated compiler
    Added debugging flags to compiler

--- server/ispc_languageserver/Compiler.cs
+++ server/ispc_languageserver/Compiler.cs
@@ -94,6 +94,14 @@ namespace ISPCLanguageServer
                     Process compilerProc = new Process();
                     compilerProc.StartInfo = _startInfo;
 
+                    // Get the doc FileName
+                    string fileName = Path.GetFileNameWithoutExtension(doc.uri.LocalPath);
+                    string localPath = Uri.UnescapeDataString(new Uri(doc.uri.ToString()).LocalPath);
+
+                    // Set names for output files
+                    compilerProc.StartInfo.Arguments += $"-h {fileName}.h -o {fileName}.ispc.obj \"{localPath}\"";
+                    _logger.Info(compilerProc.StartInfo.Arguments);
+
                     // compile the file
                     try
                     {
@@ -107,7 +115,7 @@ namespace ISPCLanguageServer
                     }
 
                     // write the file to stdin
-                    compilerProc.StandardInput.WriteLine(doc.text);
+                    //compilerProc.StandardInput.WriteLine(doc.text);
 
                     // flush and close stdin
                     compilerProc.StandardInput.Flush();
@@ -139,6 +147,7 @@ namespace ISPCLanguageServer
                     // close the process
                     compilerProc.Close();
                     compilerProc = null;
+                    UpdateStartInfo();
                 }
             }
         }
@@ -242,7 +251,7 @@ namespace ISPCLanguageServer
         private static void UpdateStartInfo()
         {
             _startInfo = new ProcessStartInfo(_compilerPath);
-            _startInfo.Arguments = $"--arch={_arch} --cpu={_CPU} --target={_target} --target-os={_TargetOS} -O3 -";
+            _startInfo.Arguments = $"--arch={_arch} --cpu={_CPU} --target={_target} --target-os={_TargetOS} -O0 -g ";
             _startInfo.WindowStyle = ProcessWindowStyle.Hidden;
             _startInfo.UseShellExecute = false;
             _startInfo.RedirectStandardError = true;
