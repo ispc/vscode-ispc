bb19419505872bbbfa7a46bc7c5a4a84471ea931
ethandav <ethan.davis@intel.com>
2023-08-28 15:32:17 -0700

    Cleaned up includes

--- server/ispc_languageserver/Program.cs
+++ server/ispc_languageserver/Program.cs
@@ -1,15 +1,4 @@
 using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Newtonsoft.Json.Linq;
-using OmniSharp.Extensions.LanguageServer.Protocol;
-using OmniSharp.Extensions.LanguageServer.Protocol.Document;
-using OmniSharp.Extensions.LanguageServer.Protocol.Models;
-using OmniSharp.Extensions.LanguageServer.Server;
-using Serilog;
 
 namespace ispc_languageserver
 {
