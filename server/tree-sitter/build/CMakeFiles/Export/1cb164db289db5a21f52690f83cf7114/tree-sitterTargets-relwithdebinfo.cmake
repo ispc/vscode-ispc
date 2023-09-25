6c9fa424090ed2ee0a999a851405752159c4974c
ethandav <ethan.davis@intel.com>
2023-09-25 11:28:42 -0700

    Added tree-sitter solution

---
+++ server/tree-sitter/build/CMakeFiles/Export/1cb164db289db5a21f52690f83cf7114/tree-sitterTargets-relwithdebinfo.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "tree-sitter::tree_sitter" for configuration "RelWithDebInfo"
+set_property(TARGET tree-sitter::tree_sitter APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(tree-sitter::tree_sitter PROPERTIES
+  IMPORTED_IMPLIB_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/tree_sitter.lib"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/bin/tree_sitter.dll"
+  )
+
+list(APPEND _cmake_import_check_targets tree-sitter::tree_sitter )
+list(APPEND _cmake_import_check_files_for_tree-sitter::tree_sitter "${_IMPORT_PREFIX}/lib/tree_sitter.lib" "${_IMPORT_PREFIX}/bin/tree_sitter.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
---
+++ server/tree-sitter/build/_deps/_fargs-subbuild/_fargs-populate-prefix/tmp/_fargs-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-src"
+  "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-build"
+  "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-subbuild/_fargs-populate-prefix"
+  "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-subbuild/_fargs-populate-prefix/tmp"
+  "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-subbuild/_fargs-populate-prefix/src/_fargs-populate-stamp"
+  "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-subbuild/_fargs-populate-prefix/src"
+  "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-subbuild/_fargs-populate-prefix/src/_fargs-populate-stamp"
+)
+
+set(configSubDirs Debug)
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-subbuild/_fargs-populate-prefix/src/_fargs-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/ethandav/OneDrive - Intel Corporation/Documents/GitHub/vscode-ispc/server/tree-sitter/build/_deps/_fargs-subbuild/_fargs-populate-prefix/src/_fargs-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
